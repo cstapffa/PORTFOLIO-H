b04aaaeaa9e73d01a0b30a0828a3c945ca2d4e62
milaCa <cami.stapff@gmail.com>
2025-05-16 17:04:52 -0300

    Fixing Details

--- js/index.js
+++ js/index.js
@@ -232,3 +232,39 @@ document.addEventListener("DOMContentLoaded", () => {
   textarea.addEventListener("input", checkContent);
   textarea.addEventListener("blur", checkContent);
 });
+
+const hoverImages = [
+  {
+    id: "about-btn",
+    defaultSrc: "https://res.cloudinary.com/dpushmfs0/image/upload/v1746408186/about-fill-grey_vzkt2l.png",
+    hoverSrc: "https://res.cloudinary.com/dpushmfs0/image/upload/v1747409303/Vector_orjfxq.png"
+  },
+  {
+    id: "projects-btn",
+    defaultSrc: "https://res.cloudinary.com/dpushmfs0/image/upload/v1746411791/projects-fill-grey_e7bqd2.png",
+    hoverSrc: "https://res.cloudinary.com/dpushmfs0/image/upload/v1747409538/Frame_47552_h85bgz.png"
+  },
+  {
+    id: "skills-btn",
+    defaultSrc: "https://res.cloudinary.com/dpushmfs0/image/upload/v1746408185/skills-fill-grey_nt3o59.png",
+    hoverSrc: "https://res.cloudinary.com/dpushmfs0/image/upload/v1747409303/Vector-3_xq8s61.png"
+  },
+  {
+    id: "contact-btn",
+    defaultSrc: "https://res.cloudinary.com/dpushmfs0/image/upload/v1746411790/contact-fill-grey_dl861o.png",
+    hoverSrc: "https://res.cloudinary.com/dpushmfs0/image/upload/v1747409539/Frame_47553_gwhlad.png"
+  }
+];
+
+hoverImages.forEach(({ id, defaultSrc, hoverSrc }) => {
+  const button = document.getElementById(id);
+  const img = button.querySelector("img");
+
+  button.addEventListener("mouseenter", () => {
+    img.src = hoverSrc;
+  });
+
+  button.addEventListener("mouseleave", () => {
+    img.src = defaultSrc;
+  });
+});
